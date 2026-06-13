0e2a44bdac7ce72d79a3c372bb9218265ea3ebb4
Pavlo Tarasenko <pashkes@gmail.com>
2026-06-13 22:23:17 +0300

    Fix GitHub Pages PWA paths

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'treadmill-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
